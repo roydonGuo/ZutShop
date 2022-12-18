1b459220b3c92f8b18bb3cfbc6fca7b1ccf828c7
roydonGuo <3133010060@qq.com>
2022-12-18 21:55:33 +0800

    RedisConstants

--- Zut-Framework/src/main/java/edu/zut/constants/RedisConstants.java
+++ Zut-Framework/src/main/java/edu/zut/constants/RedisConstants.java
@@ -2,10 +2,19 @@ package edu.zut.constants;
 
 public class RedisConstants {
 
-    public static final String LOGIN_USER_KEY="login:user:";
-    public static final String LOGIN_ADMIN_KEY="login:admin:";
+    public static final String LOGIN_USER_KEY = "login:user:";
+    //后台登录角色redis前缀
+    public static final String LOGIN_ADMIN_KEY = "login:admin:";
 
-    public static final String ARTICLE_VIEWCOUNT="article:viewCount";
+    //商品列表
+    public static final String GOODS_LIST_KEY = "goods:list:";
+    //商品列表存储过期时间
+    public static final Integer GOODS_LIST_TTL = 60;//1小时
+
+    //今日热销商品排行
+    public static final String TODAY_GOODS_KEY = "goods:today:";
+    //商品列表存储过期时间
+    public static final Integer TODAY_GOODS_TTL = 24*60;//24小时
 
 //    public static final String LOGIN_CODE_KEY = "login:code:";
 //    public static final Long LOGIN_CODE_TTL = 2L;//存在时间
