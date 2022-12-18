f6f0bd8cce052ae93af88d7bc3cba3da8a722b34
roydonGuo <3133010060@qq.com>
2022-12-18 21:56:01 +0800

    goods

--- Zut-Framework/src/main/java/edu/zut/service/impl/GoodsServiceImpl.java
+++ Zut-Framework/src/main/java/edu/zut/service/impl/GoodsServiceImpl.java
@@ -7,7 +7,14 @@ import edu.zut.domain.ResponseResult;
 import edu.zut.domain.entity.Goods;
 import edu.zut.mapper.GoodsMapper;
 import edu.zut.service.GoodsService;
+import edu.zut.utils.RedisCache;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+import static edu.zut.constants.RedisConstants.TODAY_GOODS_KEY;
+import static edu.zut.constants.RedisConstants.TODAY_GOODS_TTL;
 
 /**
  * (Goods)表服务实现类
@@ -18,16 +25,20 @@ import org.springframework.stereotype.Service;
 @Service("goodsService")
 public class GoodsServiceImpl extends ServiceImpl<GoodsMapper, Goods> implements GoodsService {
 
+    @Resource
+    private RedisCache redisCache;
+
     @Override
     public ResponseResult goodList(Integer pageNum, Integer pageSize) {
 
         LambdaQueryWrapper<Goods> queryWrapper = new LambdaQueryWrapper<>();
         //按优先级排序
         queryWrapper.orderByDesc(Goods::getPriority);
-
         Page<Goods> page = page(new Page<>(pageNum, pageSize));
+        //TODO 将查询出的商品保存到redis
 
         return ResponseResult.okResult(page);
+
     }
 
     @Override
@@ -47,11 +58,19 @@ public class GoodsServiceImpl extends ServiceImpl<GoodsMapper, Goods> implements
 
     @Override
     public Page<Goods> todayGoodList(Integer pageNum, Integer pageSize) {
-        LambdaQueryWrapper<Goods> queryWrapper = new LambdaQueryWrapper<>();
-        //按创建时间倒序排序
-        queryWrapper.orderByDesc(Goods::getCreatedTime);
-        return page(new Page<>(pageNum, pageSize), queryWrapper);
-
+        //查询redis
+        Page<Goods> todayGoodsList = redisCache.getCacheObject(TODAY_GOODS_KEY + pageNum);
+        if (todayGoodsList == null) {
+            LambdaQueryWrapper<Goods> queryWrapper = new LambdaQueryWrapper<>();
+            //按创建时间倒序排序
+            queryWrapper.orderByDesc(Goods::getCreatedTime);
+            Page<Goods> goodsPage = page(new Page<>(pageNum, pageSize), queryWrapper);
+            //TODO 存入redis
+            redisCache.setCacheObject(TODAY_GOODS_KEY + pageNum,goodsPage,TODAY_GOODS_TTL, TimeUnit.MINUTES);
+            return goodsPage;
+        }else {
+            return todayGoodsList;
+        }
     }
 }
 
